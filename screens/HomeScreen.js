72bd7096f58d7b8f11db80dca56ca21ef683efc8
Alan Secundo <alan_secundo@hotmail.com>
2021-05-28 18:37:39 -0300

    HomeScreen adaptada para fazer uma chamada na PokeApi e renderizar os pokemons dinâmicamente, commit feito mas a tarefa não está 100% completa

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,8 @@
-import React from "react";
-import { View, Image, Text, StyleSheet, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, ScrollView } from "react-native";
 import PokeCard from '../components/PokeCard';
 import PokeList from '../helpers/pokemons';
+import axios from 'axios';
 
 const styles = StyleSheet.create({
     container: {
@@ -11,6 +12,19 @@ const styles = StyleSheet.create({
 });
 
 export default function HomeScreen() {
+
+    const [pokeList, setPokeList] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0')
+        .then(response => {
+            setPokeList(response.data.results)
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }, []);
+
     return (
         <ScrollView style={styles.container}>
             {
